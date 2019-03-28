6c2e6e8e695ca34a570ce86aea63abb6a78c68e4
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-03-28 10:22:35 +0000

    Updated library.

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-function base64Encode(string) {
-    if (typeof btoa === 'function') {
-        return btoa(string);
-    }
-    if (typeof Buffer === 'function') {
-        return Buffer.from(string).toString('base64');
-    }
-    throw new Error('No way to convert to base64');
-}
-
 var defaultApiUrl = 'https://api.automationcloud.net';
 var defaultVaultUrl = 'https://vault.automationcloud.net';
-var defaultFetch = self.fetch && self.fetch.bind(self);
+var defaultFetch = typeof self !== 'undefined' && self.fetch && self.fetch.bind(self);
+var base64Encode;
 
-function fetch(url, fetchApi, token, opts) {
+if (typeof btoa === 'function') {
+    base64Encode = function(string) {
+        return btoa(string);
+    };
+} else if (typeof Buffer === 'function') {
+    base64Encode = function(string) {
+        return Buffer.from(string).toString('base64');
+    };
+} else {
+    throw new Error('No way to convert to base64.');
+}
+
+// TODO: Does service-api handle array parameters?
+function createSearch(parameters) {
+    if (!parameters) {
+        return '';
+    }
+
+    var query = [];
+
+    Object.keys(parameters).forEach(function(key) {
+        if (parameters[key] !== void 0) {
+            query.push(encodeURIComponent(key) + '=' + encodeURIComponent(parameters[key]));
+        }
+    });
+
+    var search = query.join('&');
+
+    return search.length ? '?' + search : '';
+}
+
+
+function fetchWrapper(url, fetch, token, opts) {
     var options = opts || {};
     var method = options.method || 'GET';
+    var query = options.query;
 
     if (!token) {
         throw new Error('No token.');
@@ -29,6 +53,7 @@ function fetch(url, fetchApi, token, opts) {
     headers['Authorization'] = 'Basic ' + base64Encode(token + ':');
 
     var body = options.body === void 0 ? void 0 : JSON.stringify(options.body);
+    var search = createSearch(query);
 
     if (typeof body === 'string') {
         headers['Content-Type'] = 'application/json';
@@ -47,7 +72,7 @@ function fetch(url, fetchApi, token, opts) {
         keepalive: false
     };
 
-    return fetchApi(url, fetchOptions)
+    return fetch(url + search, fetchOptions)
         .then(function(response) {
             if (!response.ok) {
                 return response.json()
@@ -65,206 +90,232 @@ function fetch(url, fetchApi, token, opts) {
         });
 }
 
-function makeJobApi(apiFetch, jobId) {
-    var callbacks = [];
-    var trackingTimeoutRef;
-    var job;
+function makeApiClient(baseUrl, fetch, token) {
+    var canonicalizedBaseiUrl = baseUrl.slice(-1) === '/' ? baseUrl : (baseUrl + '/');
 
-    const jobApi = {
-        get: function() {
-            return apiFetch(`jobs/${jobId}`);
+    function apiFetch(path, options) {
+        return fetchWrapper(canonicalizedBaseiUrl + path, fetch, token, options);
+    }
+
+    var api = {
+        raw: function(path, options) {
+            return apiFetch(path, options);
         },
-        cancel: function() {
-            return apiFetch(`jobs/${jobId}/cancel`, { method: 'POST' });
+        getServices: function() {
+            return apiFetch('services');
         },
-        reset: function() {
-            return apiFetch(`jobs/${jobId}/reset`, { method: 'POST' });
+        getService: function(serviceId) {
+            return apiFetch('services/' + serviceId);
         },
-        createInput: function({ key, stage, data }) {
-            return apiFetch(`jobs/${jobId}/inputs`, { method: 'POST', body: { jobId, key, stage, data } });
+        getJobs: function(query) {
+            return apiFetch('jobs', { query: query });
         },
-        getOutputs: function({ key, stage }) {
-            return apiFetch(`jobs/${jobId}${key ? `/${key}${stage ? `/${stage}` : ''}` : ''}`);
+        createJob: function(fields) {
+            return apiFetch('jobs', { method: 'POST', body: fields });
         },
-        getScreenshots: function() {
-            return apiFetch(`jobs/${jobId}/screenshots`);
+        getJob: function(jobId) {
+            return apiFetch('jobs/' + jobId);
         },
-        getScreenshot: function({ id, ext }) {
-            return apiFetch(`jobs/${jobId}/screenshots/${id}.${ext}`, { parse: false })
+        cancelJob: function(jobId) {
+            return apiFetch('jobs/' + jobId + '/cancel', { method: 'POST' });
+        },
+        resetJob: function(jobId) {
+            return apiFetch('jobs/' + jobId + '/reset', { method: 'POST' });
+        },
+        createJobInput: function(jobId, data, key, stage) {
+            return apiFetch('jobs/' + jobId + '/inputs', { method: 'POST', body: { key, stage, data } });
+        },
+        getJobOutputs: function(jobId, key, stage) {
+            var path = 'jobs/' + jobId;
+
+            if (key) {
+                path += '/' + key;
+
+                if (stage) {
+                    path += '/' + stage;
+                }
+            }
+
+            return apiFetch(path);
+        },
+        getJobScreenshots: function(jobId) {
+            return apiFetch('jobs/' + jobId + '/screenshots');
+        },
+        getJobScreenshot: function(jobId, id, ext) {
+            return apiFetch('jobs/' + jobId + '/screenshots/' + id + '.' + ext, { parse: false })
                 .then(res => res.blob());
         },
-        getMimoLogs: function() {
-            return apiFetch(`jobs/${jobId}/mimo-logs`);
+        getJobMimoLogs: function(jobId) {
+            return apiFetch('jobs/' + jobId + '/mimo-logs');
         },
-        track: function(callback) {
-            callbacks.add(callback);
-
-            if (!trackingTimeoutRef) {
-                poll(1000);
-            }
+        getJobEndUser: function(jobId) {
+            return apiFetch('jobs/' + jobId + '/end-user');
         },
-        untrack: function(callback) {
-            callbacks.delete(callback);
-
-            if (!callbacks.size) {
-                clearTimeout(trackingTimeoutRef);
-                trackingTimeoutRef = null;
-            }
+        trackJob: function(jobId, callback) {
+            return poll(jobId, callback, 1000); // TODO: EventSource or WebSocket.
         }
     };
 
-    // TODO: Use an events service.
-    function poll(dt) {
-        trackingTimeoutRef = setTimeout(function() {
-            jobApi.get()
+    function delay(t) {
+        return new Promise(function(resolve) {
+            setTimeout(resolve, t);
+        });
+    }
+
+    function poll(jobId, callback, dt) {
+        var job;
+
+        function run() {
+            return delay(dt)
+                .then(function() {
+                    return api.getJob(jobId);
+                })
                 .then(function(updated) {
-                    if (!trackingTimeoutRef) {
-                        return;
-                    }
-
-                    if (!job) {
-                        job = updated;
-                    }
-
-                    if (updated.state !== job.state) {
-                        for (const callback of callbacks) {
-                            callback(updated, job);
-                        }
+                    if (!job || updated.state !== job.state) {
+                        callback(updated, job);
                     }
 
                     job = updated;
-                })
-                .catch(function(error) {
-                    console.error(error);
-                })
-                .then(function() {
-                    poll(dt);
+
+                    if (job.state === 'success') {
+                        return;
+                    }
+
+                    if (job.state === 'fail') {
+                        throw new Error('Job failed.');
+                    }
+
+                    return run();
                 });
-        }, dt);
+        }
+
+        return run();
     }
 
-    return jobApi;
+    return api;
 }
 
-export function makeEndUserApi(options) {
+function makeVaultClient(baseUrl, fetch, token) {
+    var canonicalizedBaseiUrl = baseUrl.slice(-1) === '/' ? baseUrl : (baseUrl + '/');
+
+    function vaultFetch(path, options) {
+        return fetchWrapper(canonicalizedBaseiUrl + path, fetch, token, options);
+    }
+
+    return {
+        vaultPan: function(pan) {
+            return vaultFetch('otp', { method: 'POST' })
+                .then(function(otp) {
+                    return vaultFetch('pan', {
+                        method: 'POST',
+                        body: {
+                            otp: otp.id,
+                            pan: pan
+                        }
+                    });
+                })
+                .then(function(pan) {
+                    return vaultFetch('pan/temporary', {
+                        method: 'POST',
+                        body: {
+                            panId: pan.id,
+                            key: pan.key
+                        }
+                    });
+                })
+                .then(function(temp) {
+                    return temp.panToken;
+                });
+        }
+    };
+}
+
+/**
+ * @param {Object} options
+ * @param {string} options.token
+ * @param {string} options.apiUrl
+ * @param {function} options.fetch
+ */
+export function createClientSdk(options) {
     if (!options || !options.token) {
-        throw new Error('No token.');
+        throw new Error('Token required.');
     }
 
     var apiUrl = options.apiUrl || defaultApiUrl;
-    var vaultUrl = options.vaultUrl || defaultVaultUrl;
-    var fetchApi = options.fetchApi || defaultFetch;
+    var fetch = options.fetch || defaultFetch;
     var token = options.token;
-    var canonicalizedApiUrl = apiUrl.slice(-1) === '/' ? apiUrl : (apiUrl + '/');
-    var canonicalizedVaultUrl = vaultUrl.slice(-1) === '/' ? vaultUrl : (vaultUrl + '/');
 
-    if (!fetchApi) {
-        throw new Error('No fetch found.');
-    }
-
-    function apiFetch(path, options) {
-        return fetch(canonicalizedApiUrl + path, fetchApi, token, options);
-    }
-
-    function vaultFetch(path, options) {
-        return fetch(canonicalizedVaultUrl + path, fetchApi, token, options);
-    }
-
-    return apiFetch('end-user')
-        .then(endUser => {
-            return {
-                job: makeJobApi(apiFetch, endUser.jobId),
-                getService: function() {
-                    return apiFetch(`services/${endUser.serviceId}`);
-                },
-                vaultPan: function(pan) {
-                    return vaultFetch('otp', { method: 'POST' })
-                        .then(function(otp) {
-                            return vaultFetch('pan', {
-                                method: 'POST',
-                                body: {
-                                    otp: otp.id,
-                                    pan: pan
-                                }
-                            });
-                        })
-                        .then(function(pan) {
-                            return vaultFetch('pan/temporary', {
-                                method: 'POST',
-                                body: {
-                                    panId: pan.id,
-                                    key: pan.key
-                                }
-                            });
-                        })
-                        .then(function(temp) {
-                            return temp.panToken;
-                        });
-                }
-            };
-        });
+    return makeApiClient(apiUrl, fetch, token);
 }
 
-export function makeClientApi(options) {
+/**
+ * @param {Object} options
+ * @param {string} options.token
+ * @param {string} options.jobId
+ * @param {string} options.serviceId
+ * @param {string} options.apiUrl
+ * @param {string} options.vaultUrl
+ * @param {function} options.fetch
+ */
+export function createEndUserSdk(options) {
     if (!options || !options.token) {
-        throw new Error('No token.');
+        throw new Error('A token required.');
     }
 
+    if (!options.jobId) {
+        throw new Error('A jobId is required.');
+    }
+
+    if (!options.serviceId) {
+        throw new Error('A serviceId is required.');
+    }
+
+    var jobId = options.jobId;
+    var serviceId = options.serviceId;
     var apiUrl = options.apiUrl || defaultApiUrl;
     var vaultUrl = options.vaultUrl || defaultVaultUrl;
-    var fetchApi = options.fetchApi || defaultFetch;
+    var fetch = options.fetch || defaultFetch;
     var token = options.token;
-    var canonicalizedApiUrl = apiUrl.slice(-1) === '/' ? apiUrl : (apiUrl + '/');
-    var canonicalizedVaultUrl = vaultUrl.slice(-1) === '/' ? vaultUrl : (vaultUrl + '/');
 
-    if (!fetchApi) {
-        throw new Error('No fetch found.');
-    }
+    var apiClient = makeApiClient(apiUrl, fetch, token);
+    var vaultClient = makeVaultClient(vaultUrl, fetch, token);
 
-    function apiFetch(path, options) {
-        return fetch(canonicalizedApiUrl + path, fetchApi, token, options);
-    }
-
-    function vaultFetch(path, options) {
-        return fetch(canonicalizedVaultUrl + path, fetchApi, token, options);
-    }
-
-    return apiFetch('client')
-        .then(function() {
-            return {
-                makeJobApi: function(jobId) {
-                    return makeJobApi(apiFetch, jobId);
-                },
-                getServices: function() {
-                    return apiFetch('services');
-                },
-                getService: function(serviceId) {
-                    return apiFetch(`services/${serviceId}`);
-                },
-                vaultPan: function(pan) {
-                    return vaultFetch('otp', { method: 'POST' })
-                        .then(function(otp) {
-                            return vaultFetch('pan', {
-                                method: 'POST',
-                                body: {
-                                    otp: otp.id,
-                                    pan: pan
-                                }
-                            });
-                        })
-                        .then(function(pan) {
-                            return vaultFetch('pan/temporary', {
-                                method: 'POST',
-                                body: {
-                                    panId: pan.id,
-                                    key: pan.key
-                                }
-                            });
-                        })
-                        .then(function(temp) {
-                            return temp.panToken;
-                        });
-                }
-            };
-        });
+    return {
+        getService: function() {
+            return apiClient.getService(serviceId);
+        },
+        getJob: function() {
+            return apiClient.getJob(jobId);
+        },
+        cancelJob: function() {
+            return apiClient.cancelJob(jobId);
+        },
+        resetJob: function(jobId) {
+            return apiClient.resetJob(jobId);
+        },
+        createJobInput: function(data, key, stage) {
+            return apiClient.createJobInput(jobId, data, key, stage);
+        },
+        getJobOutputs: function(key, stage) {
+            return apiClient.getJobOutputs(jobId, key, stage);
+        },
+        getJobScreenshots: function() {
+            return apiClient.getJobScreenshots(jobId);
+        },
+        getJobScreenshot: function(id, ext) {
+            return apiClient.getJobScreenshot(jobId, id, ext);
+        },
+        getJobMimoLogs: function() {
+            return apiClient.getJobMimoLogs(jobId);
+        },
+        getJobEndUser: function() {
+            return apiClient.getJobEndUser(jobId);
+        },
+        trackJob: function(callback) {
+            return apiClient.trackJob(jobId, callback);
+        },
+        vaultPan: function(pan) {
+            return vaultClient.vaultPan(pan);
+        }
+    };
 }
