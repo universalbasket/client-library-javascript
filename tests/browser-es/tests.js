a5c180a72dc8d77edb0160a417f453abfcd217ce
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-10-03 12:32:59 +0100

    Moves ES module import to a before call in ES tests.

--- tests/browser-es/tests.js
+++ tests/browser-es/tests.js
@@ -1,16 +1,23 @@
-import * as clientLibrary from '../../index.js';
-
 mocha.setup('bdd');
 
 describe('client-library', function() {
+    let mod;
+
+    before(function() {
+        return import('../../index.js')
+            .then(function(result) {
+                mod = result;
+            });
+    });
+
     it('exports a "createClientSdk" method', function() {
-        if (typeof clientLibrary.createClientSdk !== 'function') {
+        if (typeof mod.createClientSdk !== 'function') {
             throw new TypeError('Not a function.');
         }
     });
 
     it('exports a "createEndUserSdk" method', function() {
-        if (typeof clientLibrary.createEndUserSdk !== 'function') {
+        if (typeof mod.createEndUserSdk !== 'function') {
             throw new TypeError('Not a function.');
         }
     });
